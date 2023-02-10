6cc709722cb9ab67535e924624380d60e675653d
juanfeReyes <juan.felipe.reyesg@gmail.com>
2023-02-09 19:37:04 -0500

    add react config and home page

---
+++ store-ui/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+import React from 'react';
+
+export const SearchButton = () => {
+    return <>
+        <Button variant='contained'>Search</Button>
+    </>
+}
+
---
+++ store-ui/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,10 @@
+import TextField from '@mui/material/TextField/TextField';
+import React from 'react';
+
+export const SearchBar = () => {
+
+    return <>
+        <TextField id="search-bar-intput" label="Search"/>
+    </>
+}
+
