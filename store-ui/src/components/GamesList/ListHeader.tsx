171f2737b85ca64b26c157b4a4852c8f2aa24190
juanfeReyes <juan.felipe.reyesg@gmail.com>
2023-04-03 14:29:08 -0500

    fix: delete inline styling

--- store-ui/src/components/GamesList/ListHeader.tsx
+++ store-ui/src/components/GamesList/ListHeader.tsx
@@ -1,4 +1,4 @@
-import { Chip, ListItem, Paper, Stack, Typography } from "@mui/material"
+import { Chip, ListItem, Paper, Stack, Typography, styled as mstyled } from "@mui/material"
 import styled from "styled-components";
 import { CreateGameModal } from "../shared/Modal/CreateGameModal";
 
@@ -12,8 +12,22 @@ display: flex;
 justify-content: 'end';
 `
 
+const PaperStyled = mstyled(Paper)(() => ({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  flexWrap: 'wrap',
+  listStyle: 'none',
+  padding: '0.5rem'
+}))
+
+const StackStyled = mstyled(Stack)(() => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  'spacing': 1
+}))
+
 const FiltersList = (props: { filters: { [key: string]: string } }) => {
-  const {filters} = props;
+  const { filters } = props;
   const areFiltersValid = () => {
     return filters && Object.values(filters).some(value => value)
   }
@@ -21,30 +35,19 @@ const FiltersList = (props: { filters: { [key: string]: string } }) => {
   return <>
     {
       areFiltersValid() &&
-      <Paper
-      elevation={1}
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          flexWrap: 'wrap',
-          listStyle: 'none',
-          padding: '0.5rem'
-        }} >
-        <Stack direction="row" spacing={1} alignItems='center'>
-          <Typography>Filters:</Typography>
+      <PaperStyled
+        elevation={1}>
+        <StackStyled>
+          <Typography>Filters: </Typography>
           {Object.entries(filters).map(filter =>
             filter[1] &&
             <Chip label={`${filter[0].toUpperCase()}:  ${filter[1]}`} />
           )}
-        </Stack>
-      </Paper>
+        </StackStyled>
+      </PaperStyled>
     }
   </>
 }
-
-/**
- * Add games filter in chip to show the filters applied
- */
 
 /**
  * 
